10a89ae079442aeef794407ce91a4dcb2217342f
LUIGIFERRARACCIO <91878510+LUIGIFERRARACCIO@users.noreply.github.com>
2021-12-12 00:03:01 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,10 +1,14 @@
+//
+// Created by LUIGIFERRARACCIO on 12/12/2021
+//
+
 #include "lib.h"
-float  numeroMassimo(const float numeri[], int dim){
-  numeri[dim];
-  float massimo=numeri[0];
-  for (int i=0; i<dim; i++){
-    if(massimo<numeri[i]){
-      massimo=numeri[i];
+float  numeroMassimo( const float numeri[], int dim){
+  numeri [dim];
+  float massimo = numeri[0];
+  for (int i=0 ; i<dim ; i++){
+    if(massimo < numeri[i]){
+      massimo = numeri[i];
     }
   }
    return msssimo;
