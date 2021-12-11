b3c3135d1006e2cc2d42064e29d50fd5d56129f2
LUIGIFERRARACCIO <91878510+LUIGIFERRARACCIO@users.noreply.github.com>
2021-12-12 00:34:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,10 @@
 #include <iostream>
 #include "lib.h"
-
 using namespace std;
-
-int main() {
-  int dim;
-  cin >> dim;
-  float numeri[dim];
-  for (int i = 0 ; i < dim ; i++ ){
-    cin >> numeri[i];
-  }
-  numeroMassimo(numeri, dim);
-  
-     cout << numeroMassimo(numeri , dim) << endl;
-  
-  
-  return 0;
+int main()
+{
+    double valoremax;
+    valoremax=vett();
+    cout<<valoremax<<endl;
+    return 0;
 }
