d38d451ab39821862525934d029c4441c27fc285
LUIGIFERRARACCIO <91878510+LUIGIFERRARACCIO@users.noreply.github.com>
2021-12-12 00:07:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 int main() {
   int dim;
-  cin>>dim;
+  cin >> dim;
   float numeri[dim];
-  for (int i=0; i<dim; i++){
-    cin>>numeri[i];
+  for (int i = 0 ; i < dim ; i++ ){
+    cin >> numeri[i];
   }
   numeroMassimo(numeri, dim);
   
-     cout<<numeroMassimo(numeri, dim)<<endl;
+     cout << numeroMassimo(numeri , dim) << endl;
   
   
   return 0;
