63079af7127c5fb167a827f5865e3a53262a20e5
LUIGIFERRARACCIO <91878510+LUIGIFERRARACCIO@users.noreply.github.com>
2021-12-12 00:33:33 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,9 +1,1 @@
-//
-// Created by LUIGIFERRARACCIO on 12/12/2021
-//
-
-#ifndef UNTITLED15_LIB_H
-#define UNTITLED15_LIB_H
-float numeroMassimo(const float numeri[], int dimensione);
-
-#endif //UNTITLED15_LIB_H
+double vett();
