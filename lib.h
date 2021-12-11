cdf0e0fcc66f92672e50d95dc643710932934fc7
LUIGIFERRARACCIO <91878510+LUIGIFERRARACCIO@users.noreply.github.com>
2021-12-11 23:50:24 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,9 @@
+//
+// Created by luigiferraraccio on 11/12/2021.
+//
 
+#ifndef UNTITLED15_LIB_H
+#define UNTITLED15_LIB_H
+float numeroMassimo(const float numeri[], int dimensione);
+
+#endif //UNTITLED15_LIB_H
